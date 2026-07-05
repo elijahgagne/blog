6ef62cd5802345cc172201e22f93f8fce55cee24
Elijah Gagne <elijah@gagnes.net>
2026-07-05 15:49:13 -0400

    soccer and tennis

---
+++ static/ben-calendar/data2025-2026.js
@@ -0,0 +1,166 @@
+const yamlString = `
+Calendar:
+  - Color: Yellow
+    - Name: Catechism
+      Date
+        - 2025-09-28
+        - 2025-11-23
+        - 2026-03-22
+    - Name: Catechism, Hosting coffee hour
+      Dates
+        - 2025-10-26
+        - 2026-01-11
+        - 2026-02-08
+        - 2026-03-08
+        - 2026-04-12
+    - Name: Catechism, blessing 8:30 am Mass, Hosting coffee hour
+      Date: 2025-09-21
+    - Name: Catechism, Marian procession for 8:30 Mass
+      Date: 2025-10-05
+    - Name: Catechism, Christmas Eve Mass Music Practice
+      Date: 2025-12-07
+    - Name: Catechism, Hosting coffee hour, Christmas Eve Mass Music Practice
+      Date: 2025-12-14
+    - Name: Catechism, First Communion Practice
+      Date: 2026-04-26
+  - Color: Yellow
+    - Name: Hosting coffee hour, Christmas Giving Project - Crafts
+      Date: 2025-11-09
+    - Name: Christmas Bazaar 9:00am-1:30pm
+      Date: 2025-11-15
+    - Name: (2 grade) First Reconciliation 2:30 pm Church Confessional
+      Date: 2025-12-06
+    - Name: Christmas Eve Mass
+      Date: 2025-12-24
+    - Name: Potlucky Bingo after 4:00 pm Mass
+      Date: 2026-01-31
+    - Name: (optional) Circle of Grace
+      Date: 2026-02-15
+    - Name: Stations of The Cross/Soup Supper 5:30pm
+      Date: 2026-03-06
+    - Name: (pending) Confirmation Mass 5:00pm Saint Anthony's Church
+      Date: 2026-05-03
+    - Name: First Communion/May Crowning 8:30am Mass
+      Date: 2026-05-10
+  - Color: Purple
+  - Color: Blue
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-09-06
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-09-09
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-09-13
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-09-16
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-09-20
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-09-23
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-09-27
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-09-30
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-10-04
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-10-07
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-10-11
+    - Name: Soccer (5:00-6:00pm Maxfield)
+      Date: 2025-10-14
+    - Name: Soccer (10:15-11:15am Maxfield)
+      Date: 2025-10-18
+  - Color: Purple
+    - Name: Soccer 5:30-6:30pm Watson
+      Dates
+        - 2026-04-30
+        - 2026-05-07
+        - 2026-05-14
+        - 2026-05-21
+        - 2026-05-28
+        - 2026-06-04
+        - 2026-06-11
+  - Color: Blue
+    - Name: Baseball Practice 5:00-6:00pm (field #3)
+      Dates
+        - 2026-04-27
+        - 2026-04-29
+        - 2026-05-04
+        - 2026-05-06
+        - 2026-05-11
+        - 2026-05-13
+        - 2026-05-18
+        - 2026-06-01
+        - 2026-06-08
+    - Name: Baseball Game Big Green vs. Wildcats 6:00pm (field #4)
+      Date: 2026-05-20
+    - Name: Baseball Game Wildcats vs. Catamounts (field #3)
+      Date: 2026-05-27
+    - Name: Baseball Game Wildcats vs. Blackbears (field #3)
+      Date: 2026-06-03
+    - Name: Baseball Game Big Green vs. Wildcats (field #3)
+      Date: 2026-06-10
+  - Color: Green
+    - Name: First Day of School
+      Date: 2025-08-28
+    - Name: Dance (Light up the night)
+      Date: 2025-10-24
+    - Name: K-5 December Concert
+      Date: 2025-12-11
+    - Name: Drama Club Performance of "CATS!" @ 6:30pm
+      Date: 2026-04-16
+    - Name: K to 2 Spring Concert
+      Date: 2026-05-13
+    - Name: Last Day
+      Date: 2026-06-17
+  - Color: Red
+    - Name: Labor Day, No School
+      Date: 2025-09-01
+    - Name: Inservice, No School
+      Dates:
+        - 2025-10-10
+        - 2026-01-19
+        - 2026-03-03
+    - Name: Indigenous People's Day, No School
+      Date: 2025-10-13
+    - Name: Conferences, No School PK-12
+      Dates:
+        - 2025-11-04
+        - 2026-03-20
+    - Name: Veteran's Day, No School
+      Date: 2025-11-11
+    - Name: Holiday Break, No School
+      Dates:
+        - 2025-11-26
+        - 2025-11-27
+        - 2025-11-28
+    - Name: Winter Break, No School
+      Dates:
+        - 2025-12-22
+        - 2025-12-23
+        - 2025-12-24
+        - 2025-12-25
+        - 2025-12-26
+        - 2025-12-29
+        - 2025-12-30
+        - 2025-12-31
+        - 2026-01-01
+        - 2026-01-02
+    - Name: February Break, No School
+      Dates:
+        - 2026-02-23
+        - 2026-02-24
+        - 2026-02-25
+        - 2026-02-26
+        - 2026-02-27
+        - 2026-03-02
+    - Name: April Break, No School
+      Dates:
+        - 2026-04-20
+        - 2026-04-21
+        - 2026-04-22
+        - 2026-04-23
+        - 2026-04-24
+    - Name: Memorial Day, No School
+      Date: 2026-05-25
+`
